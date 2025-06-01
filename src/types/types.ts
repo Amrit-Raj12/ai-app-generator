a7ad257dd5973575d6e53ca174d0196392ed15d2
Amrit-Raj12 <amrit.raj1224@gmail.com>
2025-06-01 11:28:14 +0530

    Dev -addd types

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+// Example usage types
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
