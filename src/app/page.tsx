d25c88d888bf73b411a31799322f64f6b3844c84
Amrit-Raj12 <amrit.raj1224@gmail.com>
2025-06-03 14:35:02 +0530

    Dev - added FInisher as background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,54 @@ export default function Home() {
     setIsDark(theme === 'dark')
   }, [theme])
 
+   useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/finisher-header.es5.min.js';
+    script.async = true;
+
+    script.onload = () => {
+      // @ts-ignore
+      new FinisherHeader({
+        count: 100,
+        size: {
+          min: 2,
+          max: 8,
+          pulse: 0
+        },
+        speed: {
+          x: { min: 0, max: 0.4 },
+          y: { min: 0, max: 0.6 }
+        },
+        colors: {
+          background: '#201e30',
+          particles: ['#fbfcca', '#d7f3fe', '#ffd0a7']
+        },
+        blending: 'overlay',
+        opacity: {
+          center: 1,
+          edge: 0
+        },
+        skew: -2,
+        shapes: ['c']
+      });
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-white dark:bg-gray-900 transition-colors">
-      <h1 className={`text-3xl font-bold text-center sm:text-5xl  ${isDark ? 'text-white' : 'text-blue-500'}`}>
-        Ai
-      </h1>
+    <>
+      {/* This div is required for finisher-header */}
+      <div className="finisher-header" style={{ position: 'fixed', inset: 0, zIndex: -1 }}></div>
 
-    </div>
+      <h1 style={{ position: 'relative', zIndex: 1, color: '#fff' }}>
+        Hello from Next.js
+      </h1>
+    </>
   );
 }
