b52c3b8013ca15b659cdd58b26ca529bdceebe9d
Amrit-Raj12 <amrit.raj1224@gmail.com>
2025-06-04 17:59:10 +0530

    Dev - CusomCursor component

---
+++ src/components/CustomCursor.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+export default function CustomCursor() {
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const moveCursor = (e: MouseEvent) => {
+      if (cursorRef.current) {
+        cursorRef.current.style.left = `${e.clientX}px`;
+        cursorRef.current.style.top = `${e.clientY}px`;
+      }
+    };
+
+    document.addEventListener('mousemove', moveCursor);
+
+    return () => {
+      document.removeEventListener('mousemove', moveCursor);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={cursorRef}
+      className="fixed w-6 h-6 bg-black rounded-full pointer-events-none z-50 transform -translate-x-1/2 -translate-y-1/2"
+    />
+  );
+}
