adb5ba21037a8a885325e529a13bd35794ecba6b
Amrit-Raj12 <amrit.raj1224@gmail.com>
2025-06-06 17:23:20 +0530

    Dev - added service section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import HeroSection from "@/components/HeroSection";
 import MarqueeSection from "@/components/MarqueeSection";
+import ServiceSection from "@/components/ServiceSection";
 import { motion, useInView } from "framer-motion";
 import { useRef } from 'react';
 
@@ -24,6 +25,15 @@ export default function Home() {
         transition={{ duration: 1, ease: "easeOut", delay: 0.3 }}
       >
         <MarqueeSection />
+      </motion.div>
+      {/* Service Section with Motion */}
+      <motion.div
+        // ref={marqueeRef}
+        initial={{ opacity: 0, x: -100, filter: "blur(8px)" }}
+        animate={isMarqueeInView ? { opacity: 1, x: 0, filter: "blur(0px)" } : {}}
+        transition={{ duration: 1, ease: "easeOut", delay: 0.3 }}
+      >
+        <ServiceSection />
       </motion.div>
     </>
   );
